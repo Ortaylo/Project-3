30edd2d8347d56e87b71fb2edc3abe4a08a21a14
Carlos Fontanez <charliefontanezis@gmail.com>
2022-07-25 15:08:47 -0400

    user api routes

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
-const { getAllUsers, getUserById, createUser, updateUser } = require('../../controllers/user-controller');
+
+const { 
+  getAllUsers, 
+  getUserById, 
+  createUser, 
+  updateUser 
+} = require('../../controllers/user-controller');
+
 
 router.route('/').get(getAllUsers).post(createUser);
-
 router.route('/:id').get(getUserById).put(updateUser)
 
 
