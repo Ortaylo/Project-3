60fd8bb6a08cc50922ae5a331e3d4226a97ab338
Owen Taylor <Owentaylor60@yahoo.com>
2022-07-28 14:09:52 -0400

    testing

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 export default  function SearchPage(){
     const [Auth,setAuth] =useState(false)
-    const [recipeData,setRecipeData] = useState({img: '', link: '', label: ''})
+    const [recipeData,setRecipeData] = useState([{img: '', link: '', label: ''}])
 
     const test = async () => {
         const hits = await fetch('https://api.edamam.com/api/recipes/v2?type=public&q=cookie&app_id=0637c2e7&app_key=d01b1197825271e3271e1c4e7c26646f&mealType=Snack&dishType=Biscuits%20and%20cookies&imageSize=SMALL&random=true')
@@ -17,34 +17,32 @@ export default  function SearchPage(){
             // localStorage.setItem('recipe-link',data.hits[0].recipe.url);
             // localStorage.setItem('recipe-name',data.hits[0].recipe.label);
             // localStorage.setItem('recipe-img',data.hits[0].recipe.image);
-            setRecipeData({img:data.hits[0].recipe.image,link:data.hits[0].recipe.shareAs,label:data.hits[0].recipe.label})
-            setAuth(true)
+            
+            
             return data.hits
         }).catch(err => console.error(err));
+        for(var i = 0;i<hits;i++){
+            setRecipeData(recipeData.push({img:hits[i].recipe.image,link:hits[i].recipe.shareAs,label:hits[i].recipe.label}))
+        }
+        setAuth(true)
         console.log('PRO',hits)
         return hits;
-
     }
     const recipes = test();
 	console.log(recipes)
     console.log('AUTH',Auth)
     const test2 = () => {
-        
-        console.log(recipes)
-        console.log(recipes.PromiseResult)
-        console.log(recipes[0])
-        console.log(recipes.Array)
         console.log(recipeData)
     }
     return(
         <div>
             <h1>Hi</h1>
             <button onClick={test2}>TEST</button>
-            {/* {recipes[1] && recipes.map(recipe => (
+            {recipeData[20] && recipeData.map(recipe => (
                 <div>
-
+                    {recipe.label}
                 </div>
-            ) */}
+            )
 
             )}
         </div>
